91f99eb5fc7b9c5e3abe514ad6968dbcd26d7c0a
Punnoii <kawpunnatchnon@gmail.com>
2025-03-22 19:48:43 +0700

    first commit

---
+++ client.py
@@ -0,0 +1,67 @@
+import pygame
+from network import Network
+from game import Player , Ball
+pygame.init()
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption( "Client" )
+
+def redrawWindow(screen,player, player2,ball):
+    screen.fill((255,255,255))
+    # add if else
+    if not(player.connected() and player2.connected()):
+        font = pygame.font.Font(None, 80)
+        text = font.render("Waiting for Player...", 1, (255,0,0), True)
+        win.blit(text, (width/2 - text.get_width()/2 , height/2 - text.get_height()/2))
+    else:
+        player.draw(screen)
+        player2.draw(screen)
+        ball.draw(screen)
+    
+    pygame.display.update()
+
+def main():
+    run = True
+    n = Network()
+    p , ball = n.getP() # รับ player ball ไป server
+    p.ready = True 
+    clock = pygame.time.Clock()
+    
+    while run:
+        clock.tick(60)
+        data = n.send((p, ball))  # send player and ball to server
+        p2, ball = data  # resive data from server
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        p.move()
+        redrawWindow(win, p, p2 , ball)
+
+def menu_screen():  
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(60)
+        win.fill((128, 128, 128))
+        font = pygame.font.SysFont("comicsans", 60)
+        text = font.render("Click to Play!", 1, (255,0,0))
+        win.blit(text, (100,200))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+    main()
+
+while True:
+    menu_screen()
---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+from _thread import *
+from game import Player , Ball
+import pickle
+import pygame
+
+server = "192.168.1.43" # change IP here
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+
+players = [Player(0,250,20,120,(255,0,0)), Player(480,250, 20,120, (0,0,255))]
+ball = Ball(250, 250,10,10,(0,0,0))
+
+def threaded_client(conn, player,ball):
+    conn.send(pickle.dumps((players[player], ball)))
+    reply = ""
+    while True:
+        try:
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data[0]
+
+            ball.move(players) # update ball position
+            
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                # send data player and ball to client
+                if player == 1:
+                    reply = (players[0],ball)
+                else:
+                    reply = (players[1],ball)
+
+                print("Received: ", data)
+                print("Sending : ", reply)
+
+            conn.sendall(pickle.dumps(reply))
+        except:
+            print(f"Error: {e}")
+            break
+
+    print("Lost connection")
+    conn.close()
+
+currentPlayer = 0
+while True:
+    
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer,ball))
+    currentPlayer += 1
