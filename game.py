1573a5bcb3c6bf4f39d8f50a0f2182114a78f936
Lulorluna <thanasak.cho@dome.tu.ac.th>
2025-03-23 21:56:39 +0700

    deng dung

--- game.py
+++ game.py
@@ -2,14 +2,15 @@ import pygame
 import sys
 import random
 
-class Player():
+
+class Player:
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.rect = pygame.Rect(x,y,width,height)
+        self.rect = pygame.Rect(x, y, width, height)
         self.vel = 3
         self.ready = False
         self.screen_width = 500
@@ -26,66 +27,67 @@ class Player():
 
         if keys[pygame.K_DOWN]:
             self.y += self.vel
-            
+
         # scope of Player
-        if self.y < 0 :
+        if self.y < 0:
             self.y = 0
         if self.y >= self.screen_height - self.height:
             self.y = self.screen_height - self.height
-        
-        
+
         self.update()
 
     def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
-    
-    # check connect 
+
+    # check connect
     def connected(self):
         return self.ready
-    
-class Ball():
+
+
+class Ball:
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.rect = pygame.Rect(x,y,width,height)
+        self.rect = pygame.Rect(x, y, width, height)
         self.speed_x = random.choice((1, -1))  # ความเร็วเริ่มต้นในแกน X
         self.speed_y = random.choice((1, -1))  # ความเร็วเริ่มต้นในแกน Y
         self.screen_width = 500
         self.screen_height = 500
-        
+
     def update(self):
         self.rect.topleft = (self.x, self.y)  # อัปเดตตำแหน่งของ rect
-    
-    def draw(self , screen):
+
+    def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
-        
-    def move(self,players):
-        
+
+    def move(self, players):
+
         self.x += self.speed_x
         self.y += self.speed_y
-        
-        if self.y <= 0 or self.y >= self.screen_height :
-            self.speed_y *= -1 
-        if self.x <= 0 or self.x >= self.screen_width :
+
+        if self.y <= 0 or self.y >= self.screen_height:
+            self.speed_y *= -1
+        if self.x <= 0 or self.x >= self.screen_width:
             self.ball_start()
-            
-        for player in players:
-            if self.rect.colliderect(player.rect):
-                self.speed_x *= -1  # เปลี่ยนทิศทางแกน x เมื่อชน
-                break
-        
-            
+
+        # for player in players:
+        #     if self.rect.colliderect(player.rect):
+        #         self.speed_x *= -1  # เปลี่ยนทิศทางแกน x เมื่อชน
+        #         break
+
+        if self.rect.colliderect(players[0].rect):
+            self.speed_x = -1  # เปลี่ยนทิศทางแกน x เมื่อชน
+
+        if self.rect.colliderect(players[1].rect):
+            self.speed_x = -1  # เปลี่ยนทิศทางแกน x เมื่อชน
+
         self.update()
-        
+
     def ball_start(self):
         self.x = 250
         self.y = 250
-        self.speed_x = random.choice((1,-1))
-        self.speed_y = random.choice((1,-1))
-        
-        
-
-    
+        self.speed_x = random.choice((1, -1))
+        self.speed_y = random.choice((1, -1))
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 from _thread import *
-from game import Player , Ball
+from game import Player, Ball
 import pickle
 import pygame
 
-server = "" # change IP here
+server = "127.0.0.1"  # change IP here
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,10 +18,11 @@ s.listen(2)
 print("Waiting for a connection, Server Started")
 
 
-players = [Player(0,250,20,120,(255,0,0)), Player(480,250, 20,120, (0,0,255))]
-ball = Ball(250, 250,10,10,(0,0,0))
+players = [Player(0, 250, 20, 120, (255, 0, 0)), Player(480, 250, 20, 120, (0, 0, 255))]
+ball = Ball(250, 250, 10, 10, (0, 0, 0))
 
-def threaded_client(conn, player,ball):
+
+def threaded_client(conn, player, ball):
     conn.send(pickle.dumps((players[player], ball)))
     reply = ""
     while True:
@@ -29,17 +30,17 @@ def threaded_client(conn, player,ball):
             data = pickle.loads(conn.recv(2048))
             players[player] = data[0]
 
-            ball.move(players) # update ball position
-            
+            ball.move(players)  # update ball position
+
             if not data:
                 print("Disconnected")
                 break
             else:
                 # send data player and ball to client
                 if player == 1:
-                    reply = (players[0],ball)
+                    reply = (players[0], ball)
                 else:
-                    reply = (players[1],ball)
+                    reply = (players[1], ball)
 
                 print("Received: ", data)
                 print("Sending : ", reply)
@@ -52,11 +53,12 @@ def threaded_client(conn, player,ball):
     print("Lost connection")
     conn.close()
 
+
 currentPlayer = 0
 while True:
-    
+
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (conn, currentPlayer,ball))
+    start_new_thread(threaded_client, (conn, currentPlayer, ball))
     currentPlayer += 1
