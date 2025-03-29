64bf1941bc318d7d388949c6afedafc057c86b48
Punnoii <165472903+Punnoii@users.noreply.github.com>
2025-03-30 01:07:04 +0700

    Score_Update

--- client.py
+++ client.py
@@ -1,39 +1,45 @@
 import pygame
 from network import Network
-from game import Player , Ball
+from game import *
+
 pygame.init()
 
 width = 500
 height = 500
 win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
 
-pygame.display.set_caption( "Client" )
-
-def redrawWindow(screen,player, player2,ball):
-    screen.fill((255,255,255))
-    # add if else
-    if not(player.connected() and player2.connected()):
-        font = pygame.font.Font(None, 80)
-        text = font.render("Waiting for Player...", 1, (255,0,0), True)
-        win.blit(text, (width/2 - text.get_width()/2 , height/2 - text.get_height()/2))
+def redrawWindow(screen, player, player2, ball, score):
+    screen.fill((255, 255, 255))
+    
+    if not (player.connected() and player2.connected()):
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render("Waiting for Player...", 1, (255, 0, 0))
+        win.blit(text, (width//2 - text.get_width()//2, height//2 - text.get_height()//2))
     else:
         player.draw(screen)
         player2.draw(screen)
         ball.draw(screen)
+        
+        font = pygame.font.SysFont("comicsans", 30)
+        text = font.render(f"Red: {score.score_player_1}", True, (255, 0, 0))
+        text_2 = font.render(f"Blue: {score.score_player_2}", True, (0, 0, 255))
+        win.blit(text, (10, 10))
+        win.blit(text_2, (width - text_2.get_width() - 10, 10))
     
     pygame.display.update()
 
 def main():
     run = True
     n = Network()
-    p , ball = n.getP() # รับ player ball ไป server
-    p.ready = True 
+    p, ball, score = n.getP()
+    p.ready = True
     clock = pygame.time.Clock()
     
     while run:
         clock.tick(60)
-        data = n.send((p, ball))  # send player and ball to server
-        p2, ball = data  # resive data from server
+        data = n.send((p, ball, score))
+        p2, ball, score = data
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -41,7 +47,7 @@ def main():
                 pygame.quit()
 
         p.move()
-        redrawWindow(win, p, p2 , ball)
+        redrawWindow(win, p, p2, ball, score)
 
 def menu_screen():  
     run = True
@@ -50,8 +56,8 @@ def menu_screen():
         clock.tick(60)
         win.fill((128, 128, 128))
         font = pygame.font.SysFont("comicsans", 60)
-        text = font.render("Click to Play!", 1, (255,0,0))
-        win.blit(text, (100,200))
+        text = font.render("Click to Play!", 1, (255, 0, 0))
+        win.blit(text, (100, 200))
         pygame.display.update()
 
         for event in pygame.event.get():
@@ -62,6 +68,10 @@ def menu_screen():
                 run = False
 
     main()
+
+while True:
+    menu_screen()
+
 
 while True:
     menu_screen()
